c860e8cfbf2c535a2e96c9a84c4b475616901662
Kalkidan Kelemework <kelemework267@gmail.com>
2025-10-21 11:51:59 +0100

    all crud operation added

--- src/main/java/com/lostfound/lostfound/controller/ItemController.java
+++ src/main/java/com/lostfound/lostfound/controller/ItemController.java
@@ -7,6 +7,7 @@ import com.lostfound.lostfound.model.Item;
 import com.lostfound.lostfound.repository.ItemRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/items")
@@ -23,8 +24,34 @@ public class ItemController {
         return itemRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Optional<Item> getItemById(@PathVariable String id){
+        return itemRepository.findById(id);
+    }
+
+   @PutMapping("/{id}")
+    public Item updateItem(@PathVariable String id, @RequestBody Item updatedItem){
+        return itemRepository.findById(id)
+            .map( item -> {
+                item.setName(id);
+                item.setDescription(id);
+                item.setLocationFound(id);
+                item.setPhotoUrl(id);
+                item.setClaimed(false);
+                return itemRepository.save(item);
+            })
+            .orElseThrow(()-> new RuntimeException("Item not found with id" + id));
+    }
+
     @PostMapping
     public Item addItem(@RequestBody Item item) {
         return itemRepository.save(item);
     }
+
+    @DeleteMapping("/{id}")
+      public String deteteItem(@PathVariable String id){
+           itemRepository.deleteById(id);
+           return "Item with id" + id + "deleted successfully!";
+      }
+    
 }
