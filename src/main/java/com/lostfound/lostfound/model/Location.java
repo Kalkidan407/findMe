80f60aee272b7e81c1ff11d2b3000492088cbb60
Kalkidan Kelemework <kelemework267@gmail.com>
2025-11-15 22:10:39 +0000

    location controller added

--- src/main/java/com/lostfound/lostfound/model/Location.java
+++ src/main/java/com/lostfound/lostfound/model/Location.java
@@ -5,10 +5,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Setter
+@Getter
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private String latitude;
+    private String longitude;
     
 }
